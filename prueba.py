6d17e46e996a9db80f60eb971a32ed3ffc6423bd
Alex <alex.ortega@alu.uclm.es>
2026-04-16 12:03:28 +0200

    Eliminadas pruebas innecesarias en la realización del proyecto.

--- prueba.py
+++
@@ -1,6 +0,0 @@
-
-# hazme una consulta al archivo cohorte_alegies.csv para obtener el numero de pacientes con alergia.
-import pandas as pd
-
-df = pd.read_csv("cohorte_alegias.csv")
-print(df["PacienteID"].nunique())
